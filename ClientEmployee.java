2f5ed60691649e5a49f80ad312da67d6b920ecac
Mitchell Rodio <56127734+MitchellRodio@users.noreply.github.com>
2021-01-22 11:48:47 -0700

    Add clientemployee.java

---
+++ ClientEmployee.java
@@ -0,0 +1,34 @@
+//Client program for Person/Employee
+
+import java.util.Scanner;
+
+public class ClientEmployee {
+
+    public static void main(String[] arg) {
+    
+        System.out.println
+
+        Scanner input = new Scanner(System.in);
+
+        String last, first, dept;
+
+        double pay_rate;
+
+        int hours;
+
+        Employee prof = new Employee("John", "Doe", 25.50, 50, "COSC"); //subclass alternate constructor invoked
+
+        Employee newEmp = new Employee(); //subclass default constructor invoked
+        
+        return lastName;
+        return firstName;
+        return department;
+        return payRate;
+        return hoursWorked;
+        
+        
+        
+
+    }
+
+}
